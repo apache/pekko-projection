320320f157f2522114af471aa5ad0d04ab8d7229
PJ Fanning <pjfanning@users.noreply.github.com>
2024-03-20 22:46:34 +0100

    upgrade scala (#113)
    * upgrade scala
    
    * remove annotation due to buld error
    
    * Update SlickProjectionSpec.scala

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -14,9 +14,9 @@ import sbt._
 
 object Dependencies {
 
-  val Scala213 = "2.13.12"
-  val Scala212 = "2.12.18"
-  val Scala3 = "3.3.1"
+  val Scala213 = "2.13.13"
+  val Scala212 = "2.12.19"
+  val Scala3 = "3.3.3"
   val Scala2Versions = Seq(Scala213, Scala212)
   val Scala2And3Versions = Scala2Versions.+:(Scala3)
 
