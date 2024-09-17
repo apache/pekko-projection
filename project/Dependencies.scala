384038b6ebf046b11b3ed5a2479f9ab2b34f7753
PJ Fanning <pjfanning@users.noreply.github.com>
2024-09-17 12:02:37 +0100

    test with RC versions of connectors-kafka and persistence-jdbc
    Update Dependencies.scala
    
    Update Dependencies.scala
    
    pekkoPersistenceJdbc = "1.1.0"
    
    remove staged deps
    
    test with RC versions of connectors-kafka and persistence-jdbc
    
    Update Dependencies.scala
    
    Update Dependencies.scala
    
    pekkoPersistenceJdbc = "1.1.0"

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -25,7 +25,7 @@ object Dependencies {
 
   object Versions {
     val pekko = PekkoCoreDependency.version
-    val pekkoPersistenceJdbc = "1.1.0-M1"
+    val pekkoPersistenceJdbc = "1.1.0"
     val pekkoPersistenceCassandra = "1.1.0-M1"
     val connectors = PekkoConnectorsDependency.version
     val connectorsKafka = PekkoConnectorsKafkaDependency.version
