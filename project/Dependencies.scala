8500988a3297de86cef30bf214ce563f49d95779
Scala Steward <scala_steward@virtuslab.com>
2026-02-12 16:26:59 +0000

    Update postgresql to 42.7.10

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -78,7 +78,7 @@ object Dependencies {
     val junit = "junit" % "junit" % Versions.junit
 
     val h2Driver = Compile.h2Driver
-    val postgresDriver = "org.postgresql" % "postgresql" % "42.7.9"
+    val postgresDriver = "org.postgresql" % "postgresql" % "42.7.10"
     val mysqlDriver = "com.mysql" % "mysql-connector-j" % "9.6.0"
     val msSQLServerDriver = "com.microsoft.sqlserver" % "mssql-jdbc" % "13.2.1.jre11"
     val oracleDriver = "com.oracle.ojdbc" % "ojdbc8" % "19.3.0.0"
