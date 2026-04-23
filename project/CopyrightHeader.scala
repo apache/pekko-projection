d85bc97720e140ceebbbd65d45ea41f8a881f313
PJ Fanning <pjfanning@users.noreply.github.com>
2026-04-23 22:44:27 +0200

    avoid deprecated StringUtils

--- project/CopyrightHeader.scala
+++ project/CopyrightHeader.scala
@@ -13,7 +13,7 @@
 
 import sbtheader.HeaderPlugin.autoImport._
 import sbtheader.{ CommentCreator, HeaderPlugin, NewLine }
-import org.apache.commons.lang3.StringUtils
+import org.apache.commons.lang3.Strings
 import sbt.Keys._
 import sbt._
 
@@ -111,12 +111,12 @@ trait CopyrightHeader extends AutoPlugin {
   })
 
   private def isApacheCopyrighted(text: String): Boolean =
-    StringUtils.containsIgnoreCase(text, "licensed to the apache software foundation (asf)") ||
-    StringUtils.containsIgnoreCase(text, "www.apache.org/licenses/license-2.0") ||
-    StringUtils.contains(text, "Apache-2.0")
+    Strings.CI.contains(text, "licensed to the apache software foundation (asf)") ||
+    Strings.CI.contains(text, "www.apache.org/licenses/license-2.0") ||
+    Strings.CS.contains(text, "Apache-2.0")
 
   private def isLightbendCopyrighted(text: String): Boolean =
-    StringUtils.containsIgnoreCase(text, "lightbend inc.")
+    Strings.CI.contains(text, "lightbend inc.")
 
   private def isValidCopyrightAnnotated(text: String): Boolean = {
     isApacheCopyrighted(text)
