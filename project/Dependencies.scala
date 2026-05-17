4794ebb62ae2e9ed11f189f45721424b918bed22
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-05-17 16:02:30 +0200

    Update hibernate-core to 7.3.5.Final (#477)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -108,7 +108,7 @@ object Dependencies {
   }
 
   object Examples {
-    val hibernate = "org.hibernate" % "hibernate-core" % "7.3.4.Final"
+    val hibernate = "org.hibernate" % "hibernate-core" % "7.3.5.Final"
 
     val pekkoClusterShardingTyped = "org.apache.pekko" %% "pekko-cluster-sharding-typed" % Versions.pekko
     val pekkoPersistenceCassandra =
