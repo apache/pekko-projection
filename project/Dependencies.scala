18f584dc4fa9521f8b7e445607eb59d8ae6f2459
Scala Steward <43047562+scala-steward@users.noreply.github.com>
2026-06-26 18:35:59 +0200

    Update logback-classic to 1.5.37 (#548)

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -38,7 +38,7 @@ object Dependencies {
     val junit = "4.13.2"
     val h2Driver = "2.4.240"
     val jackson = "2.21.4" // this should match the version of jackson used by pekko-serialization-jackson
-    val logback = "1.5.35"
+    val logback = "1.5.37"
   }
 
   object Compile {
